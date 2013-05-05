65601b5578be95968123d9e26b71ea39765d310a
Ian Lucas <ian.lucas@gmail.com>
2013-05-05 17:18:03 +0800

    changed to copy the rpm to a temp directory first

--- scripts/marklogic/manifests/init.pp
+++ scripts/marklogic/manifests/init.pp
@@ -1,20 +1,28 @@
 # TODO: change MarkLogic service to specify restart and status rather than puppet defaults
 class marklogic (
+    $ml_install_file = "MarkLogic-6.0-2.3.x86_64.rpm"
 ) {
 
-
-# Check if MarkLogic is installed, install if not
-# TODO: source is fetching from puppet master, change to specify hostname
-  package { 'MarkLogic':
-      ensure => present,
-      provider => "rpm",
-      source => "http://developer.marklogic.com/download/binaries/6.0/MarkLogic-6.0-3.x86_64.rpm",
+  file {"${ml_install_file}":
+    path   => "/tmp/${ml_install_file}",
+    ensure => present,
+    source => "puppet:///modules/marklogic/${ml_install_file}",
   }
 
 
-# Check if MarkLogic service is running, start if not
-# hasrestart — Whether to use the init script’s restart command instead of stop+start. 
-# hasstatus — Whether to use the init script’s status command instead of grepping the process table. 
+  # Check if MarkLogic is installed, install if not
+  # TODO: source is fetching from puppet master, change to specify hostname
+  package { 'MarkLogic':
+    ensure => present,
+    provider => "rpm",
+    require => File["${ml_install_file}"],
+    source => File["${ml_install_file}"],
+  }
+
+
+  # Check if MarkLogic service is running, start if not
+  # hasrestart — Whether to use the init script’s restart command instead of stop+start. 
+  # hasstatus — Whether to use the init script’s status command instead of grepping the process table. 
 
   service { 'MarkLogic':
     ensure     => running,
@@ -24,7 +32,7 @@ class marklogic (
     require => Package['MarkLogic'],
   }
 
-  
+
 
 }
 
