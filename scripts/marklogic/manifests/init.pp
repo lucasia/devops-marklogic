4e2c27e53e2a3b9f5de7893fdf61a0ec6250e2ea
Ian Lucas <ian.lucas@gmail.com>
2013-05-05 11:33:41 +0800

    check if MarkLogic is running, start if not

--- scripts/marklogic/manifests/init.pp
+++ scripts/marklogic/manifests/init.pp
@@ -1,3 +1,4 @@
+# TODO: change MarkLogic service to specify restart and status rather than puppet defaults
 class marklogic (
 ) {
 
@@ -7,12 +8,19 @@ class marklogic (
       source => "puppet://localhost/modules/marklogic/MarkLogic-6.0-2.3.x86_64.rpm",
   }
 
-  service { '/etc/init.d/MarkLogic':
+
+# 
+# hasrestart — Whether to use the init script’s restart command instead of stop+start. 
+# hasstatus — Whether to use the init script’s status command instead of grepping the process table. 
+
+  service { 'MarkLogic':
     ensure     => running,
     enable     => true,
     hasrestart => true,
-    hasstatus  => true,
+    hasstatus  => false,
   }
+
+  
 
 }
 
